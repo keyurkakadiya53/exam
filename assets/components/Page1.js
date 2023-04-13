455bcb73a69b5c25a723de619a8da1234ac0bcb2
Shruti Jasoliya <112627992+shrutijasoliya@users.noreply.github.com>
2023-04-13 18:38:31 +0530

    6_38

--- assets/components/Page1.js
+++ assets/components/Page1.js
@@ -10,7 +10,7 @@ import {
   View,
 } from 'react-native';
 import CheckBox from 'react-native-check-box';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import UserImage from './../rec/user1.png';
 import Tab1Image from './../rec/tab1.png';
 import Tab2Image from './../rec/tab2.png';
@@ -58,14 +58,14 @@ const page1 = () => {
   const [list, setlist] = useState([]);
   const MsgItem = (item, index) => (
     <View style={styles.msgView}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={item.image} style={{width: 30, height: 30}}></Image>
-          <View style={{marginLeft: 5}}>
-            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Image source={item.image} style={{ width: 30, height: 30 }}></Image>
+          <View style={{ marginLeft: 5 }}>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }}>
               {item.name}
             </Text>
-            <Text style={{color: 'white', fontSize: 10}}>{item.email}</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>{item.email}</Text>
           </View>
         </View>
         <View>
@@ -74,7 +74,7 @@ const page1 = () => {
             onClick={() => {
               setdata(prev => {
                 return prev.map((val, i) =>
-                  i == index ? {...val, isChecked: !val.isChecked} : val,
+                  i == index ? { ...val, isChecked: !val.isChecked } : val,
                 );
               });
             }}
@@ -83,18 +83,60 @@ const page1 = () => {
         </View>
       </View>
       <View>
-        <Text style={{color: 'white', fontSize: 10, marginTop: 5}}>
+        <Text style={{ color: 'white', fontSize: 10, marginTop: 5 }}>
           {item.discription}
         </Text>
       </View>
-      <View style={{marginTop: 5, flexDirection: 'row', alignItems: 'center'}}>
-        <Image source={TimeLogo} style={{width: 12, height: 12}}></Image>
-        <Text style={{color: 'white', fontSize: 11, marginLeft: 5}}>
+      <View style={{ marginTop: 5, flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={TimeLogo} style={{ width: 12, height: 12 }}></Image>
+        <Text style={{ color: 'white', fontSize: 11, marginLeft: 5 }}>
           {item.datetime}
         </Text>
       </View>
     </View>
   );
+
+  const MsgItem2 = (item, index) => {
+    return ischecked ? (
+      <View style={styles.msgView}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flexDirection: 'row' }}>
+            <Image source={item.image} style={{ width: 30, height: 30 }}></Image>
+            <View style={{ marginLeft: 5 }}>
+              <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 14 }}>
+                {item.name}
+              </Text>
+              <Text style={{ color: 'white', fontSize: 10 }}>{item.email}</Text>
+            </View>
+          </View>
+          <View>
+            <CheckBox
+              isChecked={item.isChecked}
+              onClick={() => {
+                setdata(prev => {
+                  return prev.map((val, i) =>
+                    i == index ? { ...val, isChecked: !val.isChecked } : val,
+                  );
+                });
+              }}
+              checkBoxColor="white"
+            />
+          </View>
+        </View>
+        <View>
+          <Text style={{ color: 'white', fontSize: 10, marginTop: 5 }}>
+            {item.discription}
+          </Text>
+        </View>
+        <View style={{ marginTop: 5, flexDirection: 'row', alignItems: 'center' }}>
+          <Image source={TimeLogo} style={{ width: 12, height: 12 }}></Image>
+          <Text style={{ color: 'white', fontSize: 11, marginLeft: 5 }}>
+            {item.datetime}
+          </Text>
+        </View>
+      </View>
+    ) : (<></>)
+  }
 
   const GoalItem = item => (
     <View
@@ -107,17 +149,16 @@ const page1 = () => {
         borderRadius: 5,
         padding: 10,
       }}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Text style={{color: 'white', fontSize: 14}}>{item.title}</Text>
-        <Text style={{color: 'white', fontSize: 12}}>{item.time}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={{ color: 'white', fontSize: 14 }}>{item.title}</Text>
+        <Text style={{ color: 'white', fontSize: 12 }}>{item.time}</Text>
       </View>
-      <Text style={{color: 'white', fontSize: 14}}>{item.desc}</Text>
+      <Text style={{ color: 'white', fontSize: 14 }}>{item.desc}</Text>
     </View>
   );
-
   const Tab1 = () => (
     <View>
-      <View style={{marginHorizontal: 20, flexDirection: 'row'}}>
+      <View style={{ marginHorizontal: 20, flexDirection: 'row' }}>
         <View
           style={{
             backgroundColor: '#50c833',
@@ -136,14 +177,13 @@ const page1 = () => {
       </View>
       <FlatList
         data={data}
-        renderItem={({item, index}) => MsgItem(item, index)}
+        renderItem={({ item, index }) => MsgItem(item, index)}
       />
     </View>
   );
-
   const Tab2 = () => (
     <View>
-      <View style={{marginHorizontal: 20, flexDirection: 'row'}}>
+      <View style={{ marginHorizontal: 20, flexDirection: 'row' }}>
         <View
           style={{
             backgroundColor: '#50c833',
@@ -160,58 +200,15 @@ const page1 = () => {
           Selected Explore
         </Text>
       </View>
-      <View style={styles.msgView}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View style={{flexDirection: 'row'}}>
-            <Image source={Logo2} style={{width: 30, height: 30}}></Image>
-            <View style={{marginLeft: 5}}>
-              <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14}}>
-                Hellena John
-              </Text>
-              <Text style={{color: 'white', fontSize: 10}}>
-                hellena.john@example.com
-              </Text>
-            </View>
-          </View>
-          {/* <View>
-            <CheckBox
-              isChecked={item.isChecked}
-              onClick={() => {
-                setdata(prev => {
-                  console.log(
-                    prev.map((val, i) =>
-                      i == index ? {...val, isChecked: !val.isChecked} : val,
-                    ),
-                  );
-                  return prev.map((val, i) =>
-                    i == index ? {...val, isChecked: !val.isChecked} : val,
-                  );
-                });
-              }}
-              checkBoxColor="white"
-            />
-          </View> */}
-        </View>
-        <View>
-          <Text style={{color: 'white', fontSize: 10, marginTop: 5}}>
-            Lorem text Lorem text Lorem text Lorem text Lorem text Lorem text
-            Lorem text Lorem text Lorem text
-          </Text>
-        </View>
-        <View
-          style={{marginTop: 5, flexDirection: 'row', alignItems: 'center'}}>
-          <Image source={TimeLogo} style={{width: 12, height: 12}}></Image>
-          <Text style={{color: 'white', fontSize: 11, marginLeft: 5}}>
-            28/06/2022 10:00 AM
-          </Text>
-        </View>
-      </View>
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => MsgItem2(item, index)}
+      />
     </View>
   );
-
   const Tab3 = () => (
     <View>
-      <View style={{marginHorizontal: 20, flexDirection: 'row'}}>
+      <View style={{ marginHorizontal: 20, flexDirection: 'row' }}>
         <View
           style={{
             backgroundColor: '#50c833',
@@ -229,8 +226,8 @@ const page1 = () => {
         </Text>
       </View>
       <View
-        style={{marginHorizontal: 20, flexDirection: 'column', marginTop: 20}}>
-        <Text style={{color: 'white', fontSize: 14}}>Title</Text>
+        style={{ marginHorizontal: 20, flexDirection: 'column', marginTop: 20 }}>
+        <Text style={{ color: 'white', fontSize: 14 }}>Title</Text>
         <TextInput
           placeholder="Enter Title"
           placeholderTextColor="#8e9aad"
@@ -242,8 +239,8 @@ const page1 = () => {
         />
       </View>
       <View
-        style={{marginHorizontal: 20, flexDirection: 'column', marginTop: 20}}>
-        <Text style={{color: 'white', fontSize: 14}}>Description</Text>
+        style={{ marginHorizontal: 20, flexDirection: 'column', marginTop: 20 }}>
+        <Text style={{ color: 'white', fontSize: 14 }}>Description</Text>
         <TextInput
           placeholder="Enter Description"
           placeholderTextColor="#8e9aad"
@@ -267,14 +264,14 @@ const page1 = () => {
           onPress={() => {
             const date = new Date();
             const time = `${date.getHours()} : ${date.getMinutes()} AM`;
-            setlist(prev => [...prev, {title: title, desc: desc, time: time}]);
+            setlist(prev => [...prev, { title: title, desc: desc, time: time }]);
             setTitle('');
             setDesc('');
             console.log('Title', title);
             console.log('Desc', desc);
             console.log(list);
           }}>
-          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
             ADD
           </Text>
         </TouchableOpacity>
@@ -283,7 +280,7 @@ const page1 = () => {
   );
   const Tab4 = () => (
     <View>
-      <View style={{marginHorizontal: 20, flexDirection: 'row'}}>
+      <View style={{ marginHorizontal: 20, flexDirection: 'row' }}>
         <View
           style={{
             backgroundColor: '#50c833',
@@ -300,16 +297,16 @@ const page1 = () => {
           Todo's
         </Text>
       </View>
-      <FlatList data={list} renderItem={({item}) => GoalItem(item)} />
+      <FlatList data={list} renderItem={({ item }) => GoalItem(item)} />
     </View>
   );
   return (
-    <View style={{backgroundColor: '#0d2b3f', flex: 1}}>
-      <View style={{marginTop: 25, paddingHorizontal: 20}}>
-        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
+    <View style={{ backgroundColor: '#0d2b3f', flex: 1 }}>
+      <View style={{ marginTop: 25, paddingHorizontal: 20 }}>
+        <Text style={{ color: 'white', fontSize: 30, fontWeight: 'bold' }}>
           Let's make today
         </Text>
-        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
+        <Text style={{ color: 'white', fontSize: 30, fontWeight: 'bold' }}>
           count
         </Text>
       </View>
@@ -321,10 +318,10 @@ const page1 = () => {
           justifyContent: 'space-between',
         }}>
         <View>
-          <Text style={{color: 'white', fontSize: 14}}>June 30th,2022</Text>
-          <Text style={{color: 'white', fontSize: 12}}>Welcome Back!</Text>
+          <Text style={{ color: 'white', fontSize: 14 }}>June 30th,2022</Text>
+          <Text style={{ color: 'white', fontSize: 12 }}>Welcome Back!</Text>
         </View>
-        <Image source={UserImage} style={{height: 40, width: 40}}></Image>
+        <Image source={UserImage} style={{ height: 40, width: 40 }}></Image>
       </View>
       <View
         style={{
@@ -337,13 +334,13 @@ const page1 = () => {
           justifyContent: 'space-between',
         }}>
         <View>
-          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>
             Cameron Williamson
           </Text>
-          <Text style={{color: 'white', fontSize: 12, marginTop: 5}}>
+          <Text style={{ color: 'white', fontSize: 12, marginTop: 5 }}>
             +91 9876543210
           </Text>
-          <Text style={{color: '#50c833', fontSize: 12, marginTop: 5}}>
+          <Text style={{ color: '#50c833', fontSize: 12, marginTop: 5 }}>
             Rs. 10,000.00
           </Text>
         </View>
@@ -365,7 +362,7 @@ const page1 = () => {
           <TouchableOpacity
             style={[
               styles.tabTouchable,
-              {backgroundColor: btnNo == 0 ? '#50c833' : '#334b5f'},
+              { backgroundColor: btnNo == 0 ? '#50c833' : '#334b5f' },
             ]}
             onPress={() => {
               setbtnNo(0);
@@ -374,7 +371,7 @@ const page1 = () => {
               source={Tab1Image}
               style={[
                 styles.tabImage,
-                {tintColor: btnNo == 0 ? 'white' : '#8e9aad'},
+                { tintColor: btnNo == 0 ? 'white' : '#8e9aad' },
               ]}></Image>
           </TouchableOpacity>
         </View>
@@ -382,7 +379,7 @@ const page1 = () => {
           <TouchableOpacity
             style={[
               styles.tabTouchable,
-              {backgroundColor: btnNo == 1 ? '#50c833' : '#334b5f'},
+              { backgroundColor: btnNo == 1 ? '#50c833' : '#334b5f' },
             ]}
             onPress={() => {
               setbtnNo(1);
@@ -391,7 +388,7 @@ const page1 = () => {
               source={Tab2Image}
               style={[
                 styles.tabImage,
-                {tintColor: btnNo == 1 ? 'white' : '#8e9aad'},
+                { tintColor: btnNo == 1 ? 'white' : '#8e9aad' },
               ]}></Image>
           </TouchableOpacity>
         </View>
@@ -399,7 +396,7 @@ const page1 = () => {
           <TouchableOpacity
             style={[
               styles.tabTouchable,
-              {backgroundColor: btnNo == 2 ? '#50c833' : '#334b5f'},
+              { backgroundColor: btnNo == 2 ? '#50c833' : '#334b5f' },
             ]}
             onPress={() => {
               setbtnNo(2);
@@ -408,7 +405,7 @@ const page1 = () => {
               source={Tab3Image}
               style={[
                 styles.tabImage,
-                {tintColor: btnNo == 2 ? 'white' : '#8e9aad'},
+                { tintColor: btnNo == 2 ? 'white' : '#8e9aad' },
               ]}></Image>
           </TouchableOpacity>
         </View>
@@ -416,7 +413,7 @@ const page1 = () => {
           <TouchableOpacity
             style={[
               styles.tabTouchable,
-              {backgroundColor: btnNo == 3 ? '#50c833' : '#334b5f'},
+              { backgroundColor: btnNo == 3 ? '#50c833' : '#334b5f' },
             ]}
             onPress={() => {
               setbtnNo(3);
@@ -425,7 +422,7 @@ const page1 = () => {
               source={Tab4Image}
               style={[
                 styles.tabImage,
-                {tintColor: btnNo == 3 ? 'white' : '#8e9aad'},
+                { tintColor: btnNo == 3 ? 'white' : '#8e9aad' },
               ]}></Image>
           </TouchableOpacity>
         </View>
